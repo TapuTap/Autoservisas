c261a36c2c84931466695a20c928cc2f1bf5aa96
Raimonda <raimondaastramskaite@gmail.com>
2023-05-30 19:39:20 +0300

    admin.py improvements

--- autoservisas/car_service/admin.py
+++ autoservisas/car_service/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("car", "date", "price")
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("license_plate", "model", "vin", "client")
+    list_filter = ("client", "model")
+    search_fields = ("license_plate", "vin")
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("title", "price")
+
+
+admin.site.register(models.Car, CarAdmin)
+admin.site.register(models.CarModel)
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderEntry)
+admin.site.register(models.Service, ServiceAdmin)
+# OrderAdmin
--- autoservisas/car_service/models.py
+++ autoservisas/car_service/models.py
@@ -1,3 +1,105 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+
 
 # Create your models here.
+
+class CarModel(models.Model):
+    brand = models.CharField(_("brand"), max_length=100, db_index=True)
+    model = models.CharField(_("model"), max_length=100, db_index=True)
+    year = models.IntegerField(_("year"), null=True, blank=True)
+    engine = models.CharField(_("engine"), max_length=100, null=True, blank=True )
+
+    class Meta:
+        ordering = ["brand", "model"]
+        verbose_name = _("car model")
+        verbose_name_plural = _("car models")
+
+    def __str__(self):
+        return f"{self.brand}, {self.model}"
+
+    def get_absolute_url(self):
+        return reverse("carmodel_detail", kwargs={"pk": self.pk})
+    
+
+class Car(models.Model):
+    license_plate = models.CharField(_("license_plate"), max_length=20, db_index=True)
+    model = models.ForeignKey(CarModel, verbose_name=_("model"), related_name="cars", on_delete=models.CASCADE)
+    vin = models.CharField(_("VIN"), max_length=17, db_index=True)
+    client = models.TextField(_("client"), max_length=100, db_index=True)
+    
+    class Meta:
+        ordering = ["license_plate"]
+        verbose_name = _("car")
+        verbose_name_plural = _("cars")
+
+    def __str__(self):
+        return self.license_plate
+
+    def get_absolute_url(self):
+        return reverse("car_detail", kwargs={"pk": self.pk})
+
+
+class Order(models.Model):
+    date = models.DateField(_("date"), auto_now=False, auto_now_add=False, null=True, blank=True)
+    car = models.ForeignKey(Car, verbose_name=_("car"), related_name="orders", on_delete=models.CASCADE)
+    price = models.DecimalField(_("price"), max_digits=10, decimal_places=2, null=True, db_index=True)
+    
+    class Meta:
+        ordering = ["date", "id"]
+        verbose_name = _("order")
+        verbose_name_plural = _("orders")
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+    def get_absolute_url(self):
+        return reverse("order_detail", kwargs={"pk": self.pk})
+
+
+class Service(models.Model):
+    title = models.CharField(_("title"), max_length=100, db_index=True )
+    price = models.DecimalField(_("price"), max_digits=10, decimal_places=2, null=True, db_index=True)
+    
+    class Meta:
+        ordering = ["title"]
+        verbose_name = _("service")
+        verbose_name_plural = _("services")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("service_detail", kwargs={"pk": self.pk})
+
+
+class OrderEntry(models.Model):
+    service = models.ForeignKey(Service, verbose_name=_("service"), related_name="order_entries", on_delete=models.CASCADE)
+    quantity = models.IntegerField(_("quantity"), null=True, db_index=True)
+    price = models.DecimalField(_("price"), max_digits=10, decimal_places=2, null=True, db_index=True)
+    order = models.ForeignKey(Order, verbose_name=_("order"), related_name="order_entries", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("order entry")
+        verbose_name_plural = _("order entries")
+
+    def __str__(self):
+        return f"OrderEntry {self.service.title}, Quantity {self.quantity}"
+
+    def get_absolute_url(self):
+        return reverse("orderentry_detail", kwargs={"pk": self.pk})
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
